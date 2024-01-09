0558e2a9021af183e22be2f157f52d9e6a9e3daa
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-01-09 13:58:22 +0100

    Initial commit

---
+++ src/main/java/ies/thiar/Prueba.java
@@ -0,0 +1,17 @@
+package ies.thiar;
+/**
+ * Prueba
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        
+        int[] primero = new int[8];
+
+        for (int i = 0; i < primero.length; i++) {
+            System.out.println(i);
+        }
+
+
+    }
+}
