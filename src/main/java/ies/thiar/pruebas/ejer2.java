f682d90fa5c2c8519c48eb15a9004b81eeb799a7
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-01-15 22:37:18 +0100

    0

---
+++ src/main/java/ies/thiar/pruebas/ejer2.java
@@ -0,0 +1,27 @@
+package ies.thiar.pruebas;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ejer2 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        ArrayList <Float>alturaAlumnos = new ArrayList<Float>();
+        
+        System.out.println("Introduce el numero: ");
+        float numero = teclado.nextFloat();
+        alturaAlumnos.add(añadirAltura(numero));
+        alturaAlumnos.add((float) 1.78);
+
+        for (Float float1 : alturaAlumnos) {
+            System.out.println(float1);
+        }
+    }
+
+
+     public static Float añadirAltura(Float altura){
+        DecimalFormat formatoDeAlturas = new DecimalFormat("#,##");
+        return Float.parseFloat(formatoDeAlturas.format(altura));
+    }
+}
