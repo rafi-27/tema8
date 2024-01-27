9f18662c216cbeeba84f66687b59a9f169fe7a02
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2024-01-27 17:58:46 +0100

    si

---
+++ src/main/java/ies/thiar/retos/ejemplo.java
@@ -0,0 +1,42 @@
+package ies.thiar.retos;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ejemplo {
+        public static void main(String[] args) {
+            int alto=5;
+            int ancho=5;
+            //Crea una matriz de alto por ancho y.
+            int[][] matriz = new int[alto][ancho];
+            
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[0].length; j++) {
+                    matriz[i][j]=ThreadLocalRandom.current().nextInt(0,5);
+                }
+            }
+
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[0].length; j++) {
+                    System.out.print(matriz[i][j]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println("Next---->");
+
+            //ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(arrayNormal));
+            int[] numeritos = {2,3,4};
+            for (int i = 0; i < numeritos.length; i++) {
+                System.out.print(numeritos[i]+" ");
+            }
+
+            //ArrayList<Integer> numerosListado = new ArrayList<>(Arrays.asList(numeritos));
+            //for (int i = 0; i < numerosListado.size(); i++) {
+              //  System.out.println(numerosListado.get(i));
+            //}
+        }
+
+
+}
--- src/main/java/ies/thiar/retos/primerReto.java
+++ src/main/java/ies/thiar/retos/primerReto.java
@@ -1,24 +1,19 @@
 package ies.thiar.retos;
-
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class primerReto {
-
-    private int[][] tablero;
-
+    //private int[][] tablero;
     public static void main(String[] args) {
-        crearTablero(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-
-        
+        primerReto tablerin = new primerReto();
+        tablerin.crearTablero(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
     }
 
 
     /**
-     * Funcion crearTablero(int alto, int ancho, int minas)
+     * metodo crearTablero(int alto, int ancho, int minas)
      */
 
-     private static void crearTablero(int alto, int ancho, int cantidadMinas){
+     private void crearTablero(int alto, int ancho, int cantidadMinas){
         if (alto<0 || ancho<0) {
             System.err.println("Error alto o ancho incorrecto.");
         }
@@ -27,21 +22,20 @@ public class primerReto {
         }
         //Crea una matriz de alto por ancho y.
         int[][] matriz = new int[alto][ancho];
+        //ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(arrayNormal));
         //coloca aleatoriamente el numero de minas una mina se representa con un -1 las demas 0
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[0].length; j++) {
-                    if (cantidadMinas != 0) {
-                    int posicion = ThreadLocalRandom.current().nextInt(0,2);
-                    if (posicion == 1) {
-                        matriz[i][j]=-1;   
-                    }
-                    }
-                    cantidadMinas--;
+        int num = 0;
+        while (num < cantidadMinas) {
+            int x = ThreadLocalRandom.current().nextInt(0,alto);
+            int y = ThreadLocalRandom.current().nextInt(0,ancho);
+            if(matriz[x][y]==0){
+                matriz[x][y]=-1;
+                cantidadMinas--;
             }
         }
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[0].length; j++) {
-                System.out.print(matriz[i][j]+"     ");
+                System.out.print(String.format("%-3d", matriz[i][j]));
             }
             System.out.println();
         }
